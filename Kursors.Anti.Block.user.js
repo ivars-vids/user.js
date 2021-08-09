ef830708cdd9fe3f72306dfe9b1ee8e6405dc1b5
ivars-vids <ivars.vids@gmail.com>
2021-08-09 14:25:08 +0300

    Update Kursors.Anti.Block.user.js

--- Kursors.Anti.Block.user.js
+++ Kursors.Anti.Block.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kursors.lv Anti-AdBlock
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Kursors.lv Anti-AdBlock
 // @author       Ivars Vids
 // @match        https://kursors.lv/*
@@ -12,4 +12,11 @@
 (function() {
     'use strict';
     unsafeWindow.eval = function(){};
+    Object.defineProperty(unsafeWindow, 'td_ad_background_click_link', {
+        get() { return undefined; }
+    });
+    document.addEventListener("DOMContentLoaded", function(){
+        document.body.className = document.body.className.replace('td-background-link','')
+        document.body.style.background = 'initial';
+    }, false);
 })();
